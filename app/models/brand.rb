a74646bbab5c5f60b798e8426dd52c87ba0428ce
christinapark0117 <christinapark0117@gmail.com>
2019-09-30 16:52:25 -0400

    update

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,16 @@
-class Brand < ActiveRecord::Base
 
+class Brand < ActiveRecord::Base
+  attr_reader :brand, :model_name, :cost
+  @@all = []
+
+  def initialize(brand, model_name, cost)
+    @brand = brand
+    @model_name = model_name
+    @cost = cost
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
