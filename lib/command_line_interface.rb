a8d6afa34281e8558b8b8ab16e33bf1eaa6d104f
heezhan <hhan1194@gmail.com>
2019-10-01 15:09:48 -0400

    cli

---
+++ lib/command_line_interface.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def welcome
+    "Style with class, have a new bag every season"
+end 
+
+def get_name_from_user
+    puts "Please enter your name"
+    customer_name = gets.chomp
+    if Customer.find_by(name: customer_name)
+        puts "You are already in the system"
+    else 
+        Customer.find_or_create_by(name: customer_name)
+        puts "You have now been added to our system"
+    end 
+end 
