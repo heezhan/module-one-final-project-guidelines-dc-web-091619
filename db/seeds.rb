542f4aa9e50fe29017361cef24c015363209f842
christinapark0117 <christinapark0117@gmail.com>
2019-10-01 11:47:37 -0400

    seeds update

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+#Brand
+chanel = Brand.find_or_create_by("Chanel")
+prada = Brand.find_or_create_by("Prada")
+chloe = Brand.find_or_create_by("Chloe")
+gucci = Brand.find_or_create_by("Gucci")
+valentino = Brand.find_or_create_by("Valentino")
+saintlaurent = Brand.find_or_create_by("Saint Laurent")
+celine = Brand.find_or_create_by("Celine")
+balenciaga = Brand.find_or_create_by("Balenciaga")
+coach = Brand.find_or_create_by("Coach")
+marcjacobs = Brand.find_or_create_by("Marc Jacobs")
+mk = Brand.find_or_create_by("Michael Kors")
+tb = Brand.find_or_create_by("Tory Burch")
+ks = Brand.find_or_create_by("Kate Spade")
+supreme = Brand.find_or_create_by("Supreme")
+
+#Customer
+christinapark = Customer.find_or_create_by(name: "Christina Park", birthday: "01.17.1990", address: "7414 Colshire Dr. Mclean, VA 22102", budget: 800.00)
+heejaehan = Customer.find_or_create_by(name: "Heejae Han", birthday: "11.18.1994", address: "1206 Ballantrae Ln. McLean, VA 22101", budget: 600.00)
+kaileylee = Customer.find_or_create_by(name: "Kailey Lee", birthday: "05.24.1991", address: "2727 S Quincy St Apt 712, Arlington, VA 22206", budget: 400.00)
+sunlee = Customer.find_or_create_by(name: "Sun Lee", birthday: "09.15.1968", address: "2090 Van Tyul Pl. Mclean VA 22102", budget: 200.00)
+seungyeonlee = Customer.find_or_create_by(name: "Seungyeon Lee", birthday: "06.15.1968", address: "1206 Ballantrae Ln. McLean, VA 22101", budget: 400.00)
+joycejin = Customer.find_or_create_by(name: "Joyce Jin", birthday: "10.28.1989", address: "8312 Wolftrap Rd. Viennam VA 22182", budget: 100.00)
+jinlee = Customer.find_or_create_by(name: "Jin Lee", birthday:"08.15.1966", address: "15021 Greymont Dr. Centerville, VA 20120", budget: 100.00)
+gracekwon = Customer.find_or_create_by(name: "Grace Kwon", birthday: "12.23.1982", address: "9865 Burke Pond Ct. Burke, VA 22015", budget: 200.00)
+daphnemamou = Customer.find_or_create_by(name: "Daphne Mamoulides", birthday: "07.01.1990", address: "533A Putnam Ave. #2 Brooklyn, NY 11221", budget: 600.00)
+c10 = Customer.find_or_create_by(name: "Heidi Albrecht", birthday: "04.17.1991", address: "4212 Dartford Dr. Seattle, WA 98101", budget: 800.00)
+
+# Handbag
+# Rental
